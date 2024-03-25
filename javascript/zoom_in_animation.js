b674d4bbf6c62aacd74174bfa2ccaa9a29ce2440
Sotiris Sotiriou <s.sotiriou62@gmail.com>
2024-03-25 16:05:44 +0200

    finished with the zoom in animation to show the second scene

--- javascript/zoom_in_animation.js
+++ javascript/zoom_in_animation.js
@@ -3,10 +3,19 @@ gsap.registerPlugin(ScrollTrigger);
 document.addEventListener("DOMContentLoaded", function() {
     const tl = gsap.timeline();
     const tl2 = gsap.timeline();
+    const tl3 = gsap.timeline();
 
     tl.to(".planets", {
         scale: 50,
         ease: "power1.inOut"
+    });
+
+    ScrollTrigger.create({
+        trigger: ".main",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: true,
+        animation: tl,
     });
 
     tl2.to('.earth-wrapper', {
@@ -20,16 +29,20 @@ document.addEventListener("DOMContentLoaded", function() {
         start: "top top",
         end: "bottom bottom",
         scrub: true,
-        animation: tl,
-        markers: true
+        animation: tl2,
+    });
+
+    tl3.to('.second-scene' , {
+        opacity: 1,
+        ease: "power1.inOut"
     });
 
     ScrollTrigger.create({
-        trigger: ".main",
-        start: "top top",
+        trigger: ".second-scene",
+        start: "top 20%",
         end: "bottom bottom",
         scrub: true,
-        animation: tl2,
+        animation: tl3,
         markers: true
     });
 });
