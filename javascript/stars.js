893a75b101c16e53c607d551d5dd308dbf470bdd
Sotiris Sotiriou <s.sotiriou62@gmail.com>
2024-03-25 10:41:23 +0200

    fixed a bug with stars init

--- javascript/stars.js
+++ javascript/stars.js
@@ -3,8 +3,8 @@ function initialiseStars(){
         let wrapper = document.getElementById('stars-wrapper');
         let star = document.createElement('div');
         star.className = 'star';
-        star.style.left = Math.random() * window.innerWidth + 'px';
-        star.style.top = Math.random() * window.innerHeight + 'px';
+        star.style.left = (Math.random() * 100) + '%';
+        star.style.top = (Math.random() * 100) + '%';
         star.style.width = Math.random() * 3 + 'px';
         star.style.height = star.style.width;
         star.style.boxShadow = '0 0 5px ' + Math.random() * 1.5 + 'px white';
